c7a1a309f77ed035c363d68b0be6bcfb118f8fc7
kiyoto <kiyoto@treasure-data.com>
2019-07-07 15:20:17 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Which TD? - Chrome Extension to know which account you are logged into
+
+This is a simple Chrome Plugin to know which TD account you are logged into.
+
+![Screenshot](./screenshot.jpg)
+
+
+## How To Use
+
+1. Downloads or clone this repository.
+2. Open URL `chrome://extensions` in Google Chrome
+3. Toggle on "Developer Mode" in the top right.
+4. Click "Load unpacked", select the repository folder downloaded in Step 1.
+5. Now you know which TD account you are logged in without navigating away from your work.
---
+++ src/inject/inject.js
@@ -0,0 +1,53 @@
+chrome.extension.sendMessage({}, function(response) {
+    var readyStateCheckInterval = setInterval(function() {
+        if (document.readyState === "complete") {
+            clearInterval(readyStateCheckInterval);
+
+            // ----------------------------------------------------------
+            // This part of the script triggers when page is done loading
+            let request = obj => {
+                return new Promise((resolve, reject) => {
+                    let xhr = new XMLHttpRequest();
+                    xhr.open(obj.method || "GET", obj.url);
+                    if (obj.headers) {
+                        Object.keys(obj.headers).forEach(key => {
+                            xhr.setRequestHeader(key, obj.headers[key]);
+                        });
+                    }
+                    xhr.onload = () => {
+                        if (xhr.status >= 200 && xhr.status < 300) {
+                            resolve(xhr.response);
+                        } else {
+                            reject(xhr.statusText);
+                        }
+                    };
+                    xhr.onerror = () => reject(xhr.statusText);
+                    xhr.send(obj.body);
+                });
+            };
+            
+            let render = accountId => {
+                let el = document.createElement('div');
+                el.innerText = 'Account: '+accountId;
+                el.style.display = 'flex';
+                el.style.alignItems = 'center';
+                el.style.padding = '3px';
+                el.style.backgroundColor = 'rgb(0,193,222)';
+                el.style.color = '#EEEEEE';
+                el.style.position = 'fixed';
+                el.style.zIndex = 9999;
+                el.style.bottom = 0; // sticky to the bottom left;
+                el.style.borderRadius = '0 5px 5px 0';
+                document.body.appendChild(el);
+            };
+            
+            request({"url":"/v4/account"}).then(data => {
+                let response = JSON.parse(data);
+                console.table(response);
+                let accountId = response.id;
+                render(accountId);
+            });
+        }
+    }, 10);
+    
+});
